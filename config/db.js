8cf268ef2f12a59c28486c6c4fbf74325d00f70a
jiraphat-puttidech <jiraphat08.puttidech@gmail.com>
2026-04-24 16:27:17 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,13 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  // เปลี่ยนจาก '127.0.0.1' เป็น process.env.DB_HOST
-  host: process.env.DB_HOST, 
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 4000, // TiDB ใช้พอร์ต 4000
-  
-  // ⚠️ สำคัญมาก: ต้องมีก้อน SSL นี้เพื่อให้ต่อ TiDB Cloud ผ่าน
+  port: process.env.DB_PORT || 4000,
+  // ✅ เพิ่มบรรทัดนี้ลงไปครับ
+  multipleStatements: true, 
   ssl: {
     minVersion: 'TLSv1.2',
     rejectUnauthorized: true
